af96d5f0ae4786b9bf6c363e09ec8b2e598a277a
Marco Valerio <mo.valerio93@gmal.com>
2022-07-17 20:06:32 +0200

    campo minato header and footer done

--- js/main.js
+++ js/main.js
@@ -3,15 +3,13 @@ L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
-
-/*
-1- crea griglia sequenziale */
+  
 
 
 const gridDom = document.getElementById('grid');
 
 
-for (let i = 0; i < 100; i++) { 
+for (let i = 1; i < 101; i++) { 
 
     const currentElement = document.createElement('div');
     currentElement.classList.add('square');
@@ -24,11 +22,7 @@ for (let i = 0; i < 100; i++) {
             this.classList.add('clicked');
         }
     }
-
     );
-
-
-
 
 
     gridDom.append(currentElement);
